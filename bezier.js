59a2dc6e89128f217e8fc2c941f36449923f630a
BirdNoFly161 <66363824+BirdNoFly161@users.noreply.github.com>
2022-11-20 23:20:35 +0100

    Add files via upload

--- bezier.js
+++ bezier.js
@@ -3,6 +3,10 @@ function setup() {
     createCanvas(900, 900);
     controlPoints.push(new ControlPoint(150,150,20))
     controlPoints.push(new ControlPoint(190,80,20))
+    controlPoints[1].velocity.x=7
+    controlPoints[1].velocity.x=7
+    controlPoints[1].acceleration.x=-2
+    controlPoints[1].acceleration.y=-2
     controlPoints.push(new ControlPoint(290,120,20))
     controlPoints.push(new ControlPoint(430,150,20))
     stroke(40,100,122);
@@ -20,6 +24,25 @@ function draw() {
         if(controlPoints[i].followingMouse==true){
             controlPoints[i].setCoordinates(mouseX, mouseY)
         }
+        controlPoints[i].x+=controlPoints[i].velocity.x
+        controlPoints[i].y+=controlPoints[i].velocity.y
+        controlPoints[i].velocity.x+=controlPoints[i].acceleration.x
+        controlPoints[i].velocity.y+=controlPoints[i].acceleration.y
+        
+        if(controlPoints[i].velocity.x<0){
+            controlPoints[i].velocity.x=0
+        }
+        if(controlPoints[i].velocity.y<0){
+            controlPoints[i].velocity.y=0
+        }
+
+        if(controlPoints[i].acceleration.x>0){
+            controlPoints[i].acceleration.x-=1
+        }
+        if(controlPoints[i].acceleration.y>0){
+            controlPoints[i].acceleration.y-=1
+        }
+
     }
     
     bezier(controlPoints[0].x,controlPoints[0].y,controlPoints[1].x,controlPoints[1].y,controlPoints[2].x,controlPoints[2].y,controlPoints[3].x,controlPoints[3].y)
@@ -52,6 +75,8 @@ class ControlPoint{
         this.y=y
         this.radius=radius
         this.followingMouse=false
+        this.velocity=createVector(0,0)
+        this.acceleration=createVector(0,0)
     }
 
     setCoordinates= function(x,y){
